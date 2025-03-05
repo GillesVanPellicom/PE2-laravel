a30cedb9d1a4fc4d072bf5542e31786a28b5a7ba
Darkgoat5 <Jordi.schoetens@hotmail.com>
2025-03-05 11:37:28 +0100

    added employees controller, added custom adult validation rule for forms, show employees in the current employee table

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Employee;
+use App\Rules\Validate_Adult;
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    public function index()
+    {
+        //https://laravel.com/docs/11.x/views#passing-data-to-views
+        $employees = Employee::all();
+        return view('employees.index', ['employees' => $employees]);
+    }
+
+    public function create()
+    {
+        return view('employees.create');
+    }
+
+    public function store(Request $request)
+    {
+        //https://stackoverflow.com/questions/47211686/list-of-laravel-validation-rules
+        //https://www.youtube.com/watch?v=q9PeXmrQLpI
+
+        /*
+            making a custom validation
+            php artisan make:rule Validate_Adult    -> creates a new rule in App\Rules
+        */
+        $employee = $request->validate([
+            'name' => 'required|string',
+            'firstname' => 'required|string',
+            'email' => 'required|email:rfc',
+            'birthdate' => ['required', 'date', 'before:today', 'after:1900-01-01', new Validate_Adult('employee')],
+            // just in case we want to be able to add employees that are not yet hired or were hired in the past
+            'hire_date' => 'nullable|after_or_equal:birthdate|before_or_equal:today',
+            'vacation_days' => 'nullable|integer',  //removing integer doesnt work
+        ],
+        [
+            'name.required' => 'Name is required',
+            'firstname.required' => 'Firstname is required',
+            'email.required' => 'Email is required',
+            'email.email' => 'Email is not valid',
+            'birthdate.required' => 'Birthdate is required',
+            'hire_date.required' => 'Hire date is required',
+            'birthdate.before' => 'The birthdate cannot be a future date.',
+            'birthdate.after' => 'The birthdate must be after 1900-01-01.',
+            //'birthdate.date' => 'The birthdate must be a date.',
+            //'hire_date.date' => 'The hire date must be a date.',
+            'hire_date.after_or_equal' => 'The hire date must be after or equal to the birthdate.',
+            'hire_date.before_or_equal' => 'The hire date cannot be a future date.',
+        ]);
+
+        $new_employee = Employee::create($employee);
+
+        return redirect()->route('employees.index');
+    }
+}
