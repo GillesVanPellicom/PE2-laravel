5c10578ebec1d36773eb9e5a1d98df7d8c541c90
Senne <sedri051@gmail.com>
2025-03-10 04:00:43 +0100

    Added packageMovement Model

---
+++ app/Models/PackageMovement.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PackageMovement extends Model
+{
+    //
+    protected $fillable = [
+        'package_id', 
+        'from_location_id', 
+        'to_location_id', 
+        'handled_by_courier_id', 
+        'vehicle_id', 
+        'departure_time', 
+        'arrival_time', 
+        'check_in_time', 
+        'check_out_time',
+    ];
+}
