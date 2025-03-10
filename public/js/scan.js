1e7d200675597a387e07029f5ccdca90c6539f7b
Senne <sedri051@gmail.com>
2025-03-10 04:01:52 +0100

    js file for the scanner

---
+++ public/js/scan.js
@@ -0,0 +1,81 @@
+let MODE = null;
+let QRActive = false;
+let scannedID = null;
+
+function chooseAction(action) {
+    MODE = action;
+    QRActive = true;
+    document.getElementById("actionModal").style.display = "none";
+}
+
+function openModal() {
+    QRActive = false;
+    scannedID = null;
+    document.getElementById("actionModal").style.display = "flex";
+}
+
+function scan(message) {
+    console.log("QR FOUND " + message);
+    if (message != scannedID) {
+        console.log("QR FOUND AND NOT FAILED " + message);
+        fetch(scanQrRoute, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "X-CSRF-TOKEN": csrf,
+            },
+            body: JSON.stringify({
+                package_id: message,
+                mode: MODE,
+            }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                alert(data.message);
+            });
+    }
+    scannedID = message;
+    qrReader.resume();
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const qrReader = new Html5Qrcode("qr-reader");
+    qrReader
+        .start(
+            {
+                facingMode: "environment",
+            },
+            {
+                fps: 10,
+                qrbox: 250,
+                aspectRatio: 1.0,
+            },
+            scan
+        )
+        .catch((err) => {});
+
+    /*
+            window.updateStatus = function(status) {
+                fetch("{{ route('package.update') }}", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                            "X-CSRF-TOKEN": "{{ csrf_token() }}"
+                        },
+                        body: JSON.stringify({
+                            packageId: scannedId,
+                            status: status
+                        })
+                    })
+                    .then(response => response.json())
+                    .then(data => {
+                        alert(data.message);
+                        closeModal();
+                    });
+            };
+
+            window.closeModal = function() {
+                packageModal.style.display = "none";
+                qrReader.resume();
+            }; */
+});
