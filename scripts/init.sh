08f207bbabbed8e847343a35b524ee2df013e6c7
Gilles Van pellicom <gilles.van.pellicom1@gmail.com>
2025-03-13 13:25:59 +0100

    Added cache clear

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Define the container name (default Laravel Sail service name)
-CONTAINER_NAME=" pe-laravel-laravel.test-1"
+CONTAINER_NAME=" pe2-laravel-laravel.test-1"
 
 # Run Composer install inside the container
 docker exec -it "$CONTAINER_NAME" composer install
@@ -14,3 +14,12 @@ docker exec -it "$CONTAINER_NAME" npm install
 
 # Run database migrations and seed
 docker exec -it "$CONTAINER_NAME" php artisan migrate:fresh --seed
+
+npm run build
+
+php artisan config:clear
+php artisan cache:clear
+php artisan route:clear
+php artisan view:clear
+
+php artisan queue:restart
