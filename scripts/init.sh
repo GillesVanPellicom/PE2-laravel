9720090acdcdeec6bd63517acd51f2395bcab239
Gilles Van pellicom <gilles.van.pellicom1@gmail.com>
2025-03-14 22:02:56 +0100

    Fix candidate #2

--- scripts/init.sh
+++ scripts/init.sh
@@ -7,16 +7,18 @@ set -e
 CONTAINER_NAME="pe2-laravel-laravel.test-1"
 
 # Run Composer install inside the container
-./vendor/sail composer install
+./vendor/bin/sail composer install
 
 # Fetch Node modules inside the container
-./vendor/sail npm install
+./vendor/bin/sail npm install
 
 # Run database migrations and seed
-./vendor/sail php artisan migrate:fresh --seed
+./vendor/bin/sail php artisan migrate:fresh --seed
 
-./vendor/sail php artisan config:clear
-./vendor/sail php artisan cache:clear
-./vendor/sail php artisan route:clear
-./vendor/sail php artisan view:clear
+./vendor/bin/sail php artisan config:clear
+./vendor/bin/sail php artisan cache:clear
+./vendor/bin/sail php artisan route:clear
+./vendor/bin/sail php artisan view:clear
+
+./vendor/bin/sail git add .
 
