704bc112a0447f5634b0158c55b353b1d08f98e6
Darkgoat5 <Jordi.schoetens@hotmail.com>
2025-03-23 02:09:17 +0100

    added small navbar for my employee pages

--- resources/views/employees/teams.blade.php
+++ resources/views/employees/teams.blade.php
@@ -1,20 +1,67 @@
 <x-app-layout>
     @section("pageName","Teams")
+
+    <div class="flex">
+        <div class="w-64 h-screen bg-gray-800 text-white">
+            <div class="px-6 py-4">
+                <h1 class="text-2xl font-semibold">Employee Dashboard</h1>
+            </div>
+            <ul class="mt-6 space-y-4">
+                <li>
+                    <a href="/employees" 
+                    class="block px-4 py-2 rounded-md hover:bg-gray-700 transition duration-300">
+                    Employees List
+                    </a>
+                </li>
+                <li>
+                    <a href="/employees/create" 
+                    class="block px-4 py-2 rounded-md hover:bg-gray-700 transition duration-300">
+                    Add Employee
+                    </a>
+                </li>
+                <li>
+                    <a href="/employees/contracts" 
+                    class="block px-4 py-2 rounded-md hover:bg-gray-700 transition duration-300">
+                    Employee Contracts
+                    </a>
+                </li>
+                <li>
+                    <a href="/employees/create-contract" 
+                    class="block px-4 py-2 rounded-md hover:bg-gray-700 transition duration-300">
+                    Add Contract
+                    </a>
+                </li>
+                <li>
+                    <a href="/employees/teams" 
+                    class="block px-4 py-2 rounded-md hover:bg-gray-700 transition duration-300">
+                    Employee Teams
+                    </a>
+                </li>
+                <li>
+                    <a href="/employees/create-team" 
+                    class="block px-4 py-2 rounded-md hover:bg-gray-700 transition duration-300">
+                    Add Team
+                    </a>
+                </li>
+                <li>
+                    <a href="/employees/functions" 
+                    class="block px-4 py-2 rounded-md hover:bg-gray-700 transition duration-300">
+                    Employee Functions
+                    </a>
+                </li>
+                <li>
+                    <a href="/employees/create-function" 
+                    class="block px-4 py-2 rounded-md hover:bg-gray-700 transition duration-300">
+                    Add Function
+                    </a>
+                </li>
+            </ul>
+        </div>
+    <div class="flex-1 p-6">
+
     <div class="container mx-auto py-10">
         <div class="text-center mb-8">
             <h1 class="text-4xl font-bold mb-4">Teams</h1>
-            <a href="{{ route('employees.index') }}" 
-               class="text-lg text-white bg-blue-500 hover:bg-blue-600 px-4 py-2 rounded shadow mr-2">
-                Home
-            </a>
-            <a href="{{ route('employees.Create') }}"
-               class="text-lg text-white bg-blue-500 hover:bg-blue-600 px-4 py-2 rounded shadow mr-2">
-                Create Employee
-            </a>
-            <a href="{{ route('employees.create_contract') }}" 
-               class="text-lg text-white bg-blue-500 hover:bg-blue-600 px-4 py-2 rounded shadow mr-2">
-                Create Contract
-            </a>
         </div>
 
         @if (session('success'))
@@ -62,5 +109,5 @@
             </table>
         </div>
     </div>
-
+</div>
 </x-app-layout>
