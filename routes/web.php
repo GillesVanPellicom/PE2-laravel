cd3b73c0e78fc58002d4486fa088a4cef7836ec0
Omran Odisee Account <omrane.hajmohamed@student.odisee.be>
2025-03-28 00:04:47 +0100

    Add routes for receiving packages and packages to return in pickup dashboard

--- routes/web.php
+++ routes/web.php
@@ -131,6 +131,9 @@ Route::post('/employees/contracts', 'App\Http\Controllers\EmployeeController@sto
 Route::get('/pickup', [PackageController::class,'index'])->name('pickup.dashboard');
 Route::get('/pickup/package/{id}', [PackageController::class,'show'])->name('pickup.package.id');
 Route::patch('/pickup/package/{id}', [PackageController::class,'setStatusPackage'])->name('pickup.dashboard.setStatusPackage');
+Route::get('pickup/dashboard/receiving-packages', [PackageController::class,'showReceivingPackages'])->name('pickup.dashboard.receiving-packages');
+Route::get('pickup/dashboard/packages-to-return', [PackageController::class,'showPackagesToReturn'])->name('pickup.dashboard.packages-to-return');
+
 
 // ======================= End Pick Up Point ====================== //
 
