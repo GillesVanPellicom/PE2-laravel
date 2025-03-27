7d9ee71e9a6f39157ba09baed9ce4d2ae8dec086
Senne De Vriendt <sedri051@gmail.com>
2025-03-27 20:02:27 +0100

    Package Info & Color fixes

--- resources/views/components/courier-modal.blade.php
+++ resources/views/components/courier-modal.blade.php
@@ -2,11 +2,11 @@
     <h2 class="text-xl font-semibold text-center mb-3"> #{{ $ref ?? 'UNKNOWN' }} </h2>
     <p class="mb-2"> {{ $sender ?? 'Unknown' }} -> {{ $reciever ?? 'Unknown' }} </p>
     <p class="mb-2"> {{ $from ?? 'Unknown' }} -> {{ $to ?? 'Unknown' }} </p>
-    <p class="mb-2"> Next Stop: {{ $nextStop ?? 'Unknown' }} </p>
-    <p class="mb-3"> Weight: {{ $weight ?? 'Unknown' }}kg </p>
+    @if (isset($nextStop)) <p class="mb-2"> Next Stop: {{ $nextStop }} </p> @endif
+    <p class="mb-3"> Weight Class: {{ $weight ?? 'Unknown' }} </p>
     <p class="mb-3"> Dimension: {{ $dimension ?? 'Unknown' }} </p>
     <p class="mb-3"> Reciever Phone: {{ $phone ?? 'Unknown' }} </p>
     <button
-        class="flex flex-col items-center justify-center bg-red-700 hover:bg-red-800 text-white p-4 rounded-xl focus:outline-none"
+        class="flex flex-col items-center justify-center bg-gray-800 hover:bg-gray-900 text-white p-4 rounded-xl focus:outline-none"
         onclick="closeInfoModal()"> Close </button>
 </div>
