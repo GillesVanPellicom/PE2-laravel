6b789e1a13741533c780a0f7a520a862bb05d20f
Gilles Van pellicom <gilles.van.pellicom1@gmail.com>
2025-03-28 12:59:36 +0100

    Fatal merge conflicts fix

--- app/Console/Commands/Tinker.php
+++ app/Console/Commands/Tinker.php
@@ -3,38 +3,32 @@
 namespace App\Console\Commands;
 
 use App\Helpers\ConsoleHelper;
-use App\Models\Location;
 use App\Models\Package;
 use App\Services\Router\Router;
-use App\Services\Router\Types\MoveOperationType;
 use App\Services\Router\Types\Node;
 use Exception;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\App;
 
 class Tinker extends Command {
-  /**
-   * The name and signature of the console command.
-   *
-   * @var string
-   */
-  protected $signature = 'gilles:tinker';
 
-  /**
-   * The console command description.
-   *
-   * @var string
-   */
-  protected $description = 'Gilles\' tinker workspace';
+protected $signature = 'gilles:tinker';
 
-  /**
-   * Execute the console command.
-   */
-  public function handle(): void {
-    /** @var Package $package */
-    $package = Package::find(2);
-    $path = $package->getMovements();
-    dd($package->getNextMovement());
+protected $description = "Gilles' tinker workspace";
 
-  }
+
+
+public function handle(): void {
+    for ($i = 1; $i <= 101; $i++) {
+      $package = Package::find($i);
+      try {
+        $path = $package->getMovements();
+        for($j = 0; $j <= 16; $j++) {
+          $package->fakeMove();
+        }
+      } catch (Exception $e) {
+        ConsoleHelper::error($e->getMessage());
+      }
+    }
+}
 }
--- app/Models/Package.php
+++ app/Models/Package.php
@@ -484,6 +484,7 @@ class Package extends Model {
           if ($timestamp === 'departure_time' && !is_null($currentMovement->next_movement)) {
             $nextMovement = $this->movements()->find($currentMovement->next_movement);
             if ($nextMovement) {
+              dd ($nextMovement);
               $this->current_location_id = $nextMovement->current_node_id;
               $this->save();
             }
