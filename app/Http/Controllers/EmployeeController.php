cb2a5db13541f442489258e9a2f2c5ff878602c0
Darkgoat5 <Jordi.schoetens@hotmail.com>
2025-04-04 00:26:02 +0200

    pdf contracts

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 
-use App\Models\{Employee, Country, City, Address, EmployeeContract, User, EmployeeFunction, Team, Role, Vacation};
+use App\Models\{Employee, Country, City, Address, EmployeeContract, User, EmployeeFunction, Team, Role, Vacation, Location};
 use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Support\Facades\Hash;
