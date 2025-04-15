cd48b100f979560fb239fb28c96a0329a8d467c6
SiebeVanHirtum <vanhirtumsiebe@gmail.com>
2025-04-15 11:58:25 +0200

    created invoice migration, model and controller

---
+++ app/Models/Invoice.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\User;
+
+class Invoice extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'company_id',
+        'discount',
+        'expiry_date',
+    ];
+
+    /**
+     * Get the company that owns the invoice.
+     */
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
