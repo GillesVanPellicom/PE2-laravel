93db08909ffe069c7af3efa7ec4f96d84eda0e52
Omran Odisee Account <omrane.hajmohamed@student.odisee.be>
2025-04-27 23:44:18 +0200

    Update profile blade to conditionally display role input based on user roles

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -29,16 +29,18 @@
                         <div class="text-red-500 text-sm mt-1">{{ $message }}</div>
                     @enderror
                 </div>
-                <div class="w-full ml-2">
-                    <label for="role" class="block text-sm font-medium text-gray-700">Role <span
-                            class="text-red-500">*</span></label>
-                    <input type="text" id="role" disabled name="role"
-                        value="{{ Auth::user()->getRoleNames()->first() }}"
-                        class="mt-1 disabled:bg-gray-200 disabled:cursor-not-allowed block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                    @error('last_name')
+                @if(Auth::user()->getRoleNames()->isNotEmpty())
+                    <div class="w-full ml-2">
+                        <label for="role" class="block text-sm font-medium text-gray-700">Role <span
+                                class="text-red-500">*</span></label>
+                        <input type="text" id="role" disabled name="role"
+                               value="{{ Auth::user()->getRoleNames()->first() }}"
+                               class="mt-1 disabled:bg-gray-200 disabled:cursor-not-allowed block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                        @error('last_name')
                         <div class="text-red-500 text-sm mt-1">{{ $message }}</div>
-                    @enderror
-                </div>
+                        @enderror
+                    </div>
+                @endif
             </div>
 
             <div class="mb-4">
@@ -157,7 +159,7 @@
 
                 $created_at = $contract ? $contract->created_at : null;
 
-                $filePath = $created_at 
+                $filePath = $created_at
                     ? "contracts/contract_" . Auth::user()->last_name . "_" . Auth::user()->first_name . "_" . $created_at . ".pdf"
                     : null;
             @endphp
