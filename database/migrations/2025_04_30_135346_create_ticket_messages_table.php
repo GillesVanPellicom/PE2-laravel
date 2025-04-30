c3c36a86e2bd2e1049e00cb37d00825d24b154ba
SiebeVanHirtum <vanhirtumsiebe@gmail.com>
2025-04-30 16:28:26 +0200

    Updated ticket message table

--- database/migrations/2025_04_30_135346_create_ticket_messages_table.php
+++ database/migrations/2025_04_30_135346_create_ticket_messages_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('ticket_messages', function (Blueprint $table) {
             $table->id();
             $table->foreignId('ticket_id')->constrained('tickets')->onDelete('cascade');
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->booleqn('is_customer_message');
             $table->text('message');
             $table->timestamps();
         });
