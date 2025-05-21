5b519ae88d5b4037959d26f912769bb2a4827973
Senne <sedri051@gmail.com>
2025-05-21 18:05:10 +0200

    fixes

--- resources/views/courierlocationchange.blade.php
+++ resources/views/courierlocationchange.blade.php
@@ -6,40 +6,15 @@
     use App\Services\Router\Types\Node;
 @endphp
 <x-app-layout>
+    HELLO WORLD
     <form method="POST" action="{{ route('courier.update.location') }}" class="p-5">
         @csrf
-        <div>
-            <label for="courier" class="block mb-2">Select Courier:</label>
-            <select id="courier" name="employee" class="border rounded px-3 py-2 w-full">
-                @php
-                    $couriers = User::role('courier')->get()->pluck('employee')->all();
-                @endphp
-                @foreach($couriers as $courier)
-                    <option value="{{ $courier->id }}">{{ $courier->user->first_name . " " . $courier->user->last_name }}</option>
-                @endforeach
-            </select>
-        </div>
-        <div class="mt-4">
-            <label for="location" class="block mb-2">Select Location:</label>
-            <select id="location" name="location" class="border rounded px-3 py-2 w-full">
-                @php
-                    $locations = [];
-                    $locs = Location::all();
-                    foreach($locs as $location){
-                        $locations[] = Node::fromId($location->id);
-                    }
-                    $locs = RouterNodes::all();
-                    foreach($locs as $location){
-                        $locations[] = Node::fromId($location->id);
-                    }
-                @endphp
-                @foreach($locations as $location)
-                    <option value="{{ $location->getID() }}">{{ $location->getDescription() }}</option>
-                @endforeach
-            </select>
-        </div>
+        <!--
+
+        -->
         <div class="mt-6">
             <button type="submit" class="bg-blue-600 text-white px-4 py-2 rounded">Change Location</button>
         </div>
     </form>
+
 </x-app-layout>
